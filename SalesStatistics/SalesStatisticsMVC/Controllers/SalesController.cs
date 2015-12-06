0989da0d6bc4803e3f953b242eeaab826c0d1f78
Ksenya Lavrinovich <lavrinovich.kg.13@gmail.com>
2015-12-06 13:42:23 +0300

    added sales controller with view (list of sales shows throught Grid.Mvc)

---
+++ SalesStatistics/SalesStatisticsMVC/Controllers/SalesController.cs
@@ -0,0 +1,24 @@
+﻿using BL.Handlers;
+using BL.Interfaces;
+using DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SalesStatisticsMVC.Controllers
+{
+    public class SalesController : Controller
+    {
+        protected static IModelHandler<Sale> _saleHandler = new SaleHandler();
+
+        // GET: Sales
+        public ActionResult Index()
+        {
+            IEnumerable<Sale> sales = _saleHandler.GetAll(x => true);
+
+            return View(sales);
+        }
+    }
+}
